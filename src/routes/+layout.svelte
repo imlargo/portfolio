3ada02c9b048a5b9a1a16b6e0917ca7d13f11765
imlargo <jlargob@unal.edu.co>
2025-05-12 20:05:55 -0500

    migrate to svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
 	import '$styles/app.css';
 	import Seo from '$src/lib/components/Seo.svelte';
 	import { analytics } from '$src/lib/client/firebase';
+	let { children } = $props();
 </script>
 
 <Seo />
 
-<slot />
+{@render children()}
