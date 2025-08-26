a7ef25004123465035af5abf33f42955170380dd
imlargo <jlargob@unal.edu.co>
2025-08-26 18:35:06 -0500

    style: restructure Hero component for improved readability and organization

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { ArrowUpRight, ArrowRight } from '@lucide/svelte';
+
+</script>
+
 <div class="py-44">
 	<div class="flex flex-col gap-y-4">
 		<span>Software Engineer</span>
@@ -7,5 +13,16 @@
 			I'm a software engineer with a passion for building web applications. 2+ years of experience
 			in full-stack development.
 		</p>
+
+		<div class="flex gap-x-4 pt-6">
+			<Button class="max-w-max rounded-full">
+				<span>Resume</span>
+				<ArrowUpRight class="size-4" />
+			</Button>
+			<Button variant="link" class="max-w-max rounded-full">
+				<span>Read more</span>
+				<ArrowRight class="size-4" />
+			</Button>
+		</div>
 	</div>
 </div>
