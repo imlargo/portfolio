74954e4193ef7ab2fe0ec028b5a8f15904c941b4
imlargo <jlargob@unal.edu.co>
2025-08-26 09:24:05 -0500

    add layout structure with Navbar and Footer components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '$styles/app.css';
 	import favicon from '$lib/assets/favicon.png';
+	import Navbar from '$lib/components/layout/Navbar.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +11,18 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div data-sveltekit-reload>
+	<div class="bg-background relative z-50 flex w-full flex-col items-center px-6 md:px-8 lg:px-12">
+		<div class="w-full max-w-screen-xl">
+			<Navbar />
+		</div>
+	</div>
+
+	<div class="flex w-full flex-col items-center px-6 md:px-8 lg:px-12">
+		<div class="flex w-full max-w-screen-xl flex-col gap-y-24 py-2">
+			{@render children?.()}
+		</div>
+	</div>
+
+	<Footer />
+</div>
