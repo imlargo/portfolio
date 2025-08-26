838a82091743f089ca08bfaa64e6a20b01829991
imlargo <jlargob@unal.edu.co>
2025-08-26 17:56:15 -0500

    feat: enhance SkillBadge component with dynamic color based on technology

--- src/lib/components/kit/SkillBadge.svelte
+++ src/lib/components/kit/SkillBadge.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getLabel, Technology } from "$lib/types/technology";
+	import { getColor, getLabel, Technology } from "$lib/types/technology";
 import { Badge } from "$lib/components/ui/badge/index.js";
     type Props = {
         tech: Technology;
@@ -9,6 +9,6 @@ import { Badge } from "$lib/components/ui/badge/index.js";
 </script>
 
 <Badge variant="outline" class="flex gap-x-2 items-center justify-center py-1">
-    <span class="flex size-3 rounded-full border-2"></span>
+    <span class="flex size-3 rounded-full border-2" style:border-color="{getColor(tech)}!important"></span>
     <span class="text-center font-semibold">{getLabel(tech)}</span>
 </Badge>
--- src/lib/types/technology.ts
+++ src/lib/types/technology.ts
@@ -1,16 +1,29 @@
 export enum Technology {
-    Typescript, 
-    Svelte, 
-    Tailwind, 
-    Javascript, 
-    HTML, 
-    CSS, 
-    SCSS, 
-    Bootstrap, 
-    Firebase, 
-    NodeJs, 
-    AppsScript, 
+    Svelte,
+    Tailwind,
+    Javascript,
+    HTML,
+    CSS,
+    SCSS,
+    Bootstrap,
+    Firebase,
+    NodeJs,
+    AppsScript,
     Python,
+    Typescript,
+    Java,
+    Golang,
+    Go,
+    Mongodb,
+    Nodejs,
+    Astro,
+    Html,
+    Css,
+    Scss,
+    Git,
+    Github,
+    Githubactions,
+    Figma,
 }
 
 export function getLabel(technology: Technology): string {
@@ -39,5 +52,52 @@ export function getLabel(technology: Technology): string {
             return "Python";
         default:
             return "Unknown";
+    }
+}
+
+export function getColor(technology: Technology) {
+    switch (technology) {
+        case Technology.Typescript:
+            return "#3178c6";
+        case Technology.Javascript:
+            return "#f0db4f";
+        case Technology.Python:
+            return "#366f9f";
+        case Technology.Java:
+            return "#f58219";
+        case Technology.Golang:
+            return "#00b4e0";
+        case Technology.Go:
+            return "#00b4e0";
+        case Technology.Mongodb:
+            return "#10aa50";
+        case Technology.Nodejs:
+            return "#81cd39";
+        case Technology.Firebase:
+            return "#ffcc30";
+        case Technology.Svelte:
+            return "#ff3e00";
+        case Technology.Astro:
+            return "#ff5904";
+        case Technology.Html:
+            return "#e14e1d";
+        case Technology.Css:
+            return "#0277bd";
+        case Technology.Scss:
+            return "#cd6799";
+        case Technology.Tailwind:
+            return "#29b8be";
+        case Technology.Bootstrap:
+            return "#7511f7";
+        case Technology.Git:
+            return "#f03c2e";
+        case Technology.Github:
+            return "#242938";
+        case Technology.Githubactions:
+            return "#2084f5";
+        case Technology.Figma:
+            return "#000000";
+        default:
+            return "#4b4b4b";
     }
 }
