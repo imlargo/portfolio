bf170779ba91c3dacd2d653d39f94cf817864ea2
imlargo <jlargob@unal.edu.co>
2025-08-26 20:10:00 -0500

    style: refactor Experience and ExperienceTimeline components for improved structure and readability

---
+++ src/lib/components/home/Experience.svelte
@@ -0,0 +1,10 @@
+<script>
+	import ExperienceTimeline from "./ExperienceTimeline.svelte";
+
+</script>
+
+<section class="flex flex-col gap-y-6">
+	<h1 class="text-4xl font-semibold">Experiencia</h1>
+
+	<ExperienceTimeline />
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,7 @@
 <script>
+	import Experience from '$lib/components/home/Experience.svelte';
 	import Hero from '$lib/components/home/Hero.svelte';
-	import Experience from '$lib/components/kit/Experience.svelte';
-	import { experience } from '$lib/constants/content';
 </script>
 
 <Hero />
-
-<section class="flex flex-col gap-y-6">
-	<h1 class="text-4xl font-semibold">Experiencia</h1>
-
-	<div class="relative">
-		<div class="flex flex-col gap-y-18 pl-8">
-			{#each experience as exp}
-			<Experience experience={exp} />
-		{/each}
-		</div>
-
-		<div class="absolute h-full w-0.5 bg-muted rounded-full top-2 left-0 line">
-		</div>
-	</div>
-</section>
-
-
-<style>
-	.line {
-		mask-image: linear-gradient(180deg,rgba(0, 0, 0, 1) 75%, rgba(255, 255, 255, 0) 100%);
-	}
-</style>
+<Experience />
