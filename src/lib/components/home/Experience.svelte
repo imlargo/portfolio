770f68b12fe2d01d5eb6b2444a3ff0c3ca9980ef
imlargo <jlargob@unal.edu.co>
2025-08-26 20:10:43 -0500

    style: add introductory paragraph to Experience section for enhanced context

--- src/lib/components/home/Experience.svelte
+++ src/lib/components/home/Experience.svelte
@@ -5,6 +5,7 @@
 
 <section class="flex flex-col gap-y-6">
 	<h1 class="text-4xl font-semibold">Experiencia</h1>
+    <p class="max-w-2xl text-muted-foreground">Desarrollo aplicaciones web innovadoras y soluciones de software personalizadas. Creando experiencias atractivas, enfocándome en la escalabilidad, el rendimiento, la calidad y pensando siempre desde la perspectiva del usuario final.</p>
 
 	<ExperienceTimeline />
 </section>
