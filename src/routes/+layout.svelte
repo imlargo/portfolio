cd9a16eb42215741c6edc090c126f5a3105c1696
imlargo <jlargob@unal.edu.co>
2025-12-20 12:43:52 -0500

    add ModeWatcher component to layout and format code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
@@ -11,6 +12,8 @@
 </svelte:head>
 
 <Toaster />
+
+<ModeWatcher defaultTheme="dark" defaultMode={'dark'} track={false} />
 
 <div class="flex w-full flex-col gap-y-6 px-4 pt-12 pb-24 md:px-12 md:pb-12 xl:px-24">
 	{@render children?.()}
