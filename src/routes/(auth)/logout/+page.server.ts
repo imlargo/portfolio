dd3a4d7c2dfc25025904af3768f9f6bbf72aae72
imlargo <jlargob@unal.edu.co>
2025-12-20 12:41:56 -0500

    _

--- src/routes/(auth)/logout/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { authCookiesManager } from '$lib/server/cookies/manager';
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load = (async ({ cookies }) => {
-	authCookiesManager.logout(cookies);
-	redirect(303, '/login');
-}) satisfies PageServerLoad;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,4 +12,6 @@
 
 <Toaster />
 
-{@render children?.()}
+<div class="flex w-full flex-col gap-y-6 px-4 pt-12 pb-24 md:px-12 md:pb-12 xl:px-24">
+	{@render children?.()}
+</div>
