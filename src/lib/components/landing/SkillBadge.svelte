e8d1dcd1f7d62549e5f854b214a1f467d229d1bc
imlargo <jlargob@unal.edu.co>
2026-01-24 12:22:53 -0500

    Refactor SkillBadge component to accept class prop and improve layout styling Enhance personal introduction in about page and update project descriptions Update inline code styling in layout CSS for consistency

--- src/lib/components/landing/SkillBadge.svelte
+++ src/lib/components/landing/SkillBadge.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { getColor, getLabel, Technology } from '$lib/assets/content/technology';
 	import { Badge } from '$lib/components/ui/badge/index.js';
+	import { cn } from '$lib/utils';
 	type Props = {
 		tech: Technology;
+		class?: string;
 	};
 
-	const { tech }: Props = $props();
+	const { tech, class: className }: Props = $props();
 </script>
 
-<Badge variant="outline" class="flex items-center justify-center gap-x-2 py-1">
+<Badge variant="outline" class={cn('flex items-center justify-center gap-x-2 py-1', className)}>
 	<span class="flex size-2.5 rounded-full border" style:border-color="{getColor(tech)}!important"
 	></span>
 	<span class="text-center font-semibold text-muted-foreground">{getLabel(tech)}</span>
